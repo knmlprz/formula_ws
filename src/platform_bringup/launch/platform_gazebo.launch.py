84d82c4370499814559d30f1bb633d96e57a1022
Bartłomiej Matuszewski <bartlomiej.matuszewski123@gmail.com>
2026-05-29 03:29:48 +0200

    feat(description): configure LiDAR params for DFRobot D500 STL-19P - Updated lidar.xacro parameters to match D500 hardware specs - Adjusted scan samples to 500 (5000Hz measuring / 10Hz update rate) - Fixed min range from 0.01m to hardware accurate 0.03m - Fixed custom world SDF loading path in platform_gazebo.launch.py - Added global namespace to /scan topic in gpu_lidar configuration

--- src/platform_bringup/launch/platform_gazebo.launch.py
+++ src/platform_bringup/launch/platform_gazebo.launch.py
@@ -118,8 +118,20 @@ def generate_launch_description():
     # Include the Gazebo launch description
     gazebo_launch = IncludeLaunchDescription(
         gazebo_pkg_launch,
-        launch_arguments={'gz_args': [f'-r -v 4 ', world_file],
+        launch_arguments={'gz_args': [f'-s -r -v 4 ', world_file],
                           'on_exit_shutdown': 'true'}.items())
+    
+
+    # Sciezka do naszego poprawionego pliku empty.sdf
+    custom_world_path = os.path.join(bringup_package_path, 'worlds', 'empty.sdf')
+   
+    # Include the Gazebo launch description
+    gazebo_launch = IncludeLaunchDescription(
+        gazebo_pkg_launch,
+        launch_arguments={'gz_args': [f'-r -v 4 ', custom_world_path],
+                        'on_exit_shutdown': 'true'}.items())
+
+
 
     robot_name = "ackermann_steering_vehicle"
     
