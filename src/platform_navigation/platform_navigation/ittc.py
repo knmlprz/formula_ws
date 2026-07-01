39d17d7c77b4c79ad982ddfb5cd375717b560cf7
Kacper <luszczkacper42@gmail.com>
2026-07-01 16:25:40 +0200

    modified ittc to stop only when an obstacle is detected

--- src/platform_navigation/platform_navigation/ittc.py
+++ src/platform_navigation/platform_navigation/ittc.py
@@ -12,11 +12,12 @@ class ittcNode(Node):
         self.get_logger().info("Ittc node initialized")
         
         self.vehicle_speed = 0.0
-        self.aeb = False
+        self.intended_speed = 0.0
 
         self.odom_sub = self.create_subscription(Odometry, "/odom", self.odom_subscriber_callback, 10)
-        self.laser_sub = self.create_subscription(LaserScan, "/scan", self.laser_scan_subscriber_callback, 10)        
+        # Zapisujemy komendy z teleop, aby wiedzieć, czy kierowca próbuje cofać
         self.teleop_sub = self.create_subscription(TwistStamped, "/ackermann_steering_controller/reference", self.teleop_callback, 10)
+        self.laser_sub = self.create_subscription(LaserScan, "/scan", self.laser_scan_subscriber_callback, 10)        
 
         self.drive_pub = self.create_publisher(TwistStamped, "/ittc/cmd_vel", 10)
 
@@ -24,8 +25,7 @@ class ittcNode(Node):
         self.vehicle_speed = msg.twist.twist.linear.x
 
     def teleop_callback(self, msg: TwistStamped):
-        if msg.twist.linear.x < 0.0:
-            self.aeb = False
+        self.intended_speed = msg.twist.linear.x
 
     def send_stop_message(self):
         stop_msg = TwistStamped()
@@ -38,18 +38,14 @@ class ittcNode(Node):
         self.drive_pub.publish(stop_msg)
 
     def laser_scan_subscriber_callback(self, msg: LaserScan):
-        if self.vehicle_speed <= 0.0 and not self.aeb:
+        if self.intended_speed < 0.0:
             return
-        
-        if self.aeb:
-            self.send_stop_message()
-            return
-        
-        self.aeb = False
 
         ranges = msg.ranges
         min_angle = msg.angle_min
         increment = msg.angle_increment
+
+        obstacle_detected = False
 
         for i in range(len(ranges)):
             r = ranges[i]
@@ -57,17 +53,19 @@ class ittcNode(Node):
                 continue 
             
             current_angle = min_angle + (i * increment)
+            
             approach_speed = self.vehicle_speed * math.cos(current_angle)
-
-            ittc = math.inf
             if approach_speed > 0: 
                 ittc = r / approach_speed
-
-            if ittc < 0.5:
-                self.aeb = True
+                if ittc < 0.5:
+                    obstacle_detected = True
+                    break 
+            
+            if abs(current_angle) < 0.5 and r < 0.3:
+                obstacle_detected = True
                 break 
 
-        if self.aeb:
+        if obstacle_detected:
             self.send_stop_message()
 
 def main(args=None):
@@ -78,7 +76,3 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
-
-
-#ros2 run lidar ittc
-#ros2 run twist_mux twist_mux --ros-args --remap cmd_vel_out:=/ackermann_steering_controller/reference --params-file src/lidar/config/twist_mux.yaml
